0c3d9095519d838876822eb5559868875515752a
elenamora <hanele96@gmail.com>
2020-05-27 18:28:50 +0200

    depl

--- heroku/app.py
+++ heroku/app.py
@@ -16,8 +16,7 @@ from models.lock import lock
 
 from flask import render_template
 
-app = Flask(__name__, static_folder="../frontend/dist/static",
-          template_folder="../frontend/dist")
+app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 environment = config['development']
